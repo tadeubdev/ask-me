e8e601bb9acd40953c08cfa070536c419c87e5ea
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-08-15 21:20:27 -0300

    refactor: configure sendMessage function

--- src/send-message.js
+++ src/send-message.js
@@ -1,5 +1,13 @@
 const sendMessage = async (client, to, message) => {
-  return await client.sendText(to, message);
+  return new Promise(async (resolve, reject) => {
+    console.log('Sending message: ', message, ' to: ', to);
+    client.sendText(to, message).catch(e => {
+      console.log('Something went wrong when sending the message', e.message);
+      reject(e);
+    }).then(() => {
+      resolve();
+    });
+  }
 }
 
 module.exports = sendMessage;
