3f06cbc905dc169fc7fe19da3b9536997d8e5af4
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-08-17 10:46:49 -0300

    refactor: make function more testable

--- src/handle-get-old-messages.js
+++ src/handle-get-old-messages.js
@@ -1,24 +1,33 @@
 const manipulateMessage = async (messages, callable) => {
   if (!messages || messages.length === 0) return;
   for (let message of messages) {
-    await callable(message);
+    callable && await callable(message);
   }
 }
 
 const sendSeenToNumber = async (client, number) => {
-  if (!number) return;
+  if (!client || !number) return;
   await client.sendSeen(number);
+}
+
+const getAllChats = async (client) => {
+  if (!client) {
+    return null;
+  }
+  return await client.getAllChatsWithMessages(true);
 }
 
 const handleGetOldMessages = async (client, callable) => {
   return new Promise(async (resolve) => {
     try {
-      const chats = await client.getAllChatsWithMessages(true);
+      const chats = await getAllChats(client);
       if (!chats || chats.length === 0) return resolve();
 
       for (let chat of chats) {
-        await manipulateMessage(chat.msgs || [], callable);
-        await sendSeenToNumber(client, chat.from);
+        const messages = chat.msgs && chat.msgs.length? chat.msgs: [];
+        const number = chat.from && chat.from.length? chat.from: null;
+        await manipulateMessage(messages, callable);
+        await sendSeenToNumber(client, number);
       }
     } catch (e) {
       console.error('Something went wrong when trying to get old messages: ', e.message);
