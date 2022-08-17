e414fc5279b2ff94a966411d631936d22b80ab23
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-08-17 10:18:31 -0300

    refactor: move all validation to only one function

--- src/handle-get-old-messages.js
+++ src/handle-get-old-messages.js
@@ -1,30 +1,21 @@
 const manipulateMessage = async (messages, callable) => {
-  
-  try{
     if (!messages || messages.length === 0) return;
     for (let message of messages) {
       await callable(message);
     }
-  }catch(e){
-    console.log(e)
-  }
- 
 }
 
 const handleGetOldMessages = async (client, callable) => {
-
-  try{
+  try {
     const chats = await client.getAllChatsWithMessages(true);
     if (!chats || chats.length === 0) return;
-    
     for (let chat of chats) {
       await manipulateMessage(chat.msgs || [], callable);
       await client.sendSeen(chat.from);
     }
-  }catch(e){
-    console.log(e)
+  } catch(e) {
+    console.log('Something went wrong when trying to get old messages: ', e.message);
   }
-
 }
 
 module.exports = handleGetOldMessages;
