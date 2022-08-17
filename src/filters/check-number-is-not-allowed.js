5e01c796510e8d3957f9f1c89daf8f96adfc8d0f
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-08-17 11:13:52 -0300

    feat: create a function to check if number is allowed

---
+++ src/filters/check-number-is-not-allowed.js
@@ -0,0 +1,10 @@
+const checkNumberIsNotAllowed = (number) => {
+  const groupNumber = process.env.GROUP_NUMBER || null;
+  const allowedNumbers = process.env.ALLOWED_NUMBERS? process.env.ALLOWED_NUMBERS.split(','): [];
+  if (groupNumber) {
+    allowedNumbers.push(groupNumber);
+  }
+  return allowedNumbers.includes(number) === false;
+}
+
+module.exports = checkNumberIsNotAllowed;
